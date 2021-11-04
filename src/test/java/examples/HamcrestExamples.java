347ccdd4e7759b881143852903f07e3157bc7799
Janis <a@a.pl>
2021-11-04 10:31:35 +0100

    zmiana na branczu po rebase

--- src/test/java/examples/HamcrestExamples.java
+++ src/test/java/examples/HamcrestExamples.java
@@ -2,9 +2,6 @@ package examples;
 
 public class HamcrestExamples {
 	private int master = 99;
-	public void should(){
-		System.out.println("zmiana na masterze");
-	}
 
 	public void should(){
 		System.out.println("true = " + true);
